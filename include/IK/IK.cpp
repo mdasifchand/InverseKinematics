8d38b10b2723ad9779a1077a5b9e297b54449922
Asif <mdasifchand@gmail.com>
2022-01-30 21:17:19 +0100

    formatted the code

--- include/IK/IK.cpp
+++ include/IK/IK.cpp
@@ -4,9 +4,10 @@
 
 #include "IK.h"
 
-trafo2d_t IK::forward_kinematics(const vector_t& q ){
+trafo2d_t IK::forward_kinematics(const vector_t & q)
+{
     // check that the joint angle vector has the correct size
-    assert( q.size() == 3 );
+    assert(q.size() == 3);
 
     // define a constant offset between two joints
     trafo2d_t link_offset = trafo2d_t::Identity();
@@ -15,68 +16,84 @@ trafo2d_t IK::forward_kinematics(const vector_t& q ){
     // define the start pose
     trafo2d_t trafo = trafo2d_t::Identity();
 
-    for(int joint_idx = 0; joint_idx < 3 ; joint_idx++ ) {
+    for (int joint_idx = 0; joint_idx < 3; joint_idx++)
+    {
         // add the rotation contributed by this joint
         trafo *= Eigen::Rotation2D<double>(q(joint_idx));
         // add the link offset to this position
-        trafo = trafo * link_offset; // T_end = R(q(1)) * Tx(q(1)) * R((q(2)) * Tx(q(2))  ......  R(q(n))* Tx((q(n)))
-    }                                //  T_end -> should give use the joint coordinates x, y, phi
-                                     // q(1) , q(2) and q(3) are basically joint angles
+        trafo = trafo * link_offset; // T_end = R(q(1)) * Tx(q(1)) * R((q(2)) *
+                                     // Tx(q(2))  ......  R(q(n))* Tx((q(n)))
+    } //  T_end -> should give use the joint coordinates x, y, phi
+      // q(1) , q(2) and q(3) are basically joint angles
     return trafo;
 }
 
-
-double IK::computeVectorNorm(const vector_t& q){
-    return sqrt( pow(q(0),2) + pow(q(1),2) + pow(q(2),2));
+double IK::computeVectorNorm(const vector_t & q)
+{
+    // compute L2 norm of the vector
+    return sqrt(pow(q(0), 2) + pow(q(1), 2) + pow(q(2), 2));
 }
 
-
-void  IK::computeJacobi(matrix_t& J, const vector_t& q_current ){
+void IK::computeJacobi(matrix_t & J, const vector_t & q_current)
+{
 
     assert(J.rows() == 2);
     assert(J.cols() == 3);
-
-    J(0,0) = cos(q_current(0)) + cos(q_current(0) + q_current(1)) + cos(q_current(0) + q_current(1) + q_current(2));
-    J(0,1) =   cos(q_current(0) + q_current(1)) + cos(q_current(0) + q_current(1) + q_current(2));
-    J(0,2) =  cos(q_current(0) + q_current(1) + q_current(2));
-    J(1,0) = -sin(q_current(0)) - sin(q_current(0) + q_current(1)) - sin(q_current(0) + q_current(1) + q_current(2));
-    J(1,1) = sin(q_current(0) + q_current(1)) - sin(q_current(0) + q_current(1) + q_current(2));
-    J(1,2) = sin(q_current(0) + q_current(1) + q_current(2));
-
+    // Create a jacobian matrix at a given joint angle configuration
+    J(0, 0) = cos(q_current(0)) + cos(q_current(0) + q_current(1)) +
+              cos(q_current(0) + q_current(1) + q_current(2));
+    J(0, 1) = cos(q_current(0) + q_current(1)) +
+              cos(q_current(0) + q_current(1) + q_current(2));
+    J(0, 2) = cos(q_current(0) + q_current(1) + q_current(2));
+    J(1, 0) = -sin(q_current(0)) - sin(q_current(0) + q_current(1)) -
+              sin(q_current(0) + q_current(1) + q_current(2));
+    J(1, 1) = sin(q_current(0) + q_current(1)) -
+              sin(q_current(0) + q_current(1) + q_current(2));
+    J(1, 2) = sin(q_current(0) + q_current(1) + q_current(2));
 }
 
-vector_t IK::inverse_kinematics(){
+vector_t IK::inverse_kinematics()
+{
     vector_t q_current = m_qstart;
     vector_t diff_theta(3);
     vector_t diff_pos(2);
     diff_theta = m_qstart;
-    matrix_t  J(2,3);
-    matrix_t pseudo_inv(3,2);
+    matrix_t J(2, 3);
+    matrix_t pseudo_inv(3, 2);
     trafo2d_t t;
-    int it =0;
+    int it = 0;
+    // create Pangolin Window
     pangolin::CreateWindowAndBind("Main", 720, 640);
     pangolin::DataLog logger;
     std::vector<std::string> legend;
+    // Add legends
     legend.emplace_back(std::string("norm"));
-    legend.emplace_back(std::string("iteration"));
+    // legend.emplace_back(std::string("iteration"));
     logger.SetLabels(legend);
-    pangolin::Plotter plotter( &logger, 0.0f, 10.0f,0.0f, 10.0f  );
-    plotter.SetBounds(0.0,1.0, 0.0,1.0);
+    // Define the limits
+    pangolin::Plotter plotter(&logger, 0.0f, 10.0f, 0.0f, 10.0f);
+    plotter.SetBounds(0.0, 1.0, 0.0, 1.0);
     plotter.Track("$i");
-    plotter.AddMarker(pangolin::Marker::Vertical,   -1000, pangolin::Marker::LessThan, pangolin::Colour::Blue().WithAlpha(0.2f) );
-    //plotter.AddMarker(pangolin::Marker::Horizontal,   100, pangolin::Marker::GreaterThan, pangolin::Colour::Red().WithAlpha(0.2f) );
+    plotter.AddMarker(pangolin::Marker::Vertical, -1000,
+                      pangolin::Marker::LessThan,
+                      pangolin::Colour::Blue().WithAlpha(0.2f));
+    // plotter.AddMarker(pangolin::Marker::Horizontal,   100,
+    // pangolin::Marker::GreaterThan, pangolin::Colour::Red().WithAlpha(0.2f) );
     pangolin::DisplayBase().AddDisplay(plotter);
-
-    while(IK::computeVectorNorm(diff_theta) > 1e-3 && !pangolin::ShouldQuit() && it < 100)
+    while (IK::computeVectorNorm(diff_theta) > 1e-3 &&
+           !pangolin::ShouldQuit() && it < 100)
     {
         computeJacobi(J, q_current);
-        // compute pseudo inverse, this is a redundant system (Jacobi is rectangular)
-        pseudo_inv =  J.transpose()*((J*J.transpose()).inverse());
-        diff_pos = m_goal.translation() - forward_kinematics(q_current).translation();
-        diff_theta = pseudo_inv* diff_pos;
+        // compute pseudo inverse, this is a redundant system (Jacobi is
+        // rectangular)
+        pseudo_inv = J.transpose() * ((J * J.transpose()).inverse());
+        diff_pos =
+            m_goal.translation() - forward_kinematics(q_current).translation();
+        diff_theta = pseudo_inv * diff_pos;
         q_current = q_current + diff_theta;
         it++;
-        std::cout << " === The norm is " << computeVectorNorm(diff_theta) << " ==== iteration is " << it << std::endl;
+        std::cout << " === The norm is " << computeVectorNorm(diff_theta)
+                  << " ==== iteration is " << it << std::endl;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         logger.Log(computeVectorNorm(diff_theta));
         // Render graph, Swap frames and Process Events
@@ -87,8 +104,8 @@ vector_t IK::inverse_kinematics(){
     return q_current;
 }
 // Checks if the value lies in the WS1
-bool checkWS(const trafo2d_t goal) {
-    //TODO
+bool checkWS(const trafo2d_t goal)
+{
+    // TODO
     return true;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include <chrono>
 #include <cmath>
-#include <vector>
+#include <iostream>
 #include <pangolin/display/display.h>
 #include <pangolin/plot/plotter.h>
+#include <thread>
+#include <vector>
 
 #include "IK.h"
 
@@ -14,17 +14,18 @@
  * An example how the inverse kinematics can be used.
  * It should not be required to change this code.
  */
-int main(){
+int main()
+{
 
     // initial conditions and desired goal
     vector_t q_start(3);
-    q_start << -0.1, -0.1, -0.1 ;
+    q_start << -0.1, -0.1, -0.1;
     std::cout << q_start << std::endl;
 
     trafo2d_t goal = trafo2d_t::Identity();
     goal.translation()(0) = 1.;
     vector_t q_val(3);
-    //q_val = inverse_kinematics(q_start, goal);
+    // q_val = inverse_kinematics(q_start, goal);
 
     IK ik(q_start, goal);
     ik.inverse_kinematics();
